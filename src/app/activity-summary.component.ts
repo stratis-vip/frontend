2be7fcba9b4516703d4af326d181714dba60c26b
Stratis Christodoulou <stratis.vip@gmail.com>
2017-09-19 14:58:43 +0300

    end of day

---
+++ src/app/activity-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ActivitySummary-selector',
+    template: '<h2>Summary</h2>'
+})
+export class ActivitySummaryComponent {
+
+
+  
+}
